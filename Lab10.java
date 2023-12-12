dd7bffa4c4bf11aa8df3b1634dda7647442802de
Dapify <136081682+Dapify@users.noreply.github.com>
2023-12-12 11:35:10 +0000

    initialise variables outside of loop in question 1

--- Lab10.java
+++ Lab10.java
@@ -13,30 +13,32 @@ public class Lab10 {
     }
 
     public static void Q1() {
+        double a, b, r;
+
         while (true) {
             System.out.println("Pick a shape: square, rectangle, circle (or 'q' to quit)");
             String input = scan.nextLine();
+
             if (input.equals("q")) {
                 return;
             }
 
             if (input.equals("square")) {
-                double a;
                 System.out.println("Enter the length of side a: ");
                 a = Double.parseDouble(scan.nextLine());
                 System.out.println("The circumference of the square is: " + a * 4);
                 System.out.println("The area of the square is: " + a * a);
 
             } else if (input.equals("rectangle")) {
-                double a, b;
+                
                 System.out.println("Enter the length of side a: ");
                 a = Double.parseDouble(scan.nextLine());
                 System.out.println("Enter the length of side b: ");
                 b = Double.parseDouble(scan.nextLine());
                 System.out.println("The circumference of the rectangle is: " + (2 * a + 2 * b));
                 System.out.println("The area of the rectangle is: " + (a * b));
+                
             } else if (input.equals("circle")) {
-                double r;
                 System.out.println("Enter the radius: ");
                 r = Double.parseDouble(scan.nextLine());
                 System.out.println("The circumference of the circle is: " + (Math.PI * r * 2));
